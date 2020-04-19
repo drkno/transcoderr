80b9642ae688e441f246a6f88507f5d4cef7fa29
Matthew Knox <matthew@makereti.co.nz>
2020-04-19 11:32:41 +1000

    Changes

--- src/backend/migrations/001-initial.sql
+++ src/backend/migrations/001-initial.sql
@@ -2,14 +2,14 @@
 -- Up
 --------------------------------------------------------------------------------
 
-CREATE TABLE States (
+CREATE TABLE JobStates (
     id          INTEGER PRIMARY KEY,
     state       TEXT    NOT NULL UNIQUE,
     name        TEXT    NOT NULL,
     description TEXT    NOT NULL
 );
 
-CREATE TABLE Job (
+CREATE TABLE Jobs (
     id          INTEGER PRIMARY KEY,
     state       TEXT    NOT NULL,
     file        TEXT    NOT NULL,
@@ -18,19 +18,57 @@ CREATE TABLE Job (
     lastFailure TEXT,
     runCount    INTEGER NOT NULL,
 
-    CONSTRAINT job_fk_state FOREIGN KEY (state)
-        REFERENCES States(state)
+    CONSTRAINT jobs_fk_state FOREIGN KEY (state)
+        REFERENCES JobStates(state)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
-CREATE TABLE Plugin (
-    id          INTEGER PRIMARY KEY,
-    name        TEXT    NOT NULL UNIQUE,
-    enabled     INTEGER NOT NULL DEFAULT 1,
-    checksum    TEXT,
+CREATE TABLE PluginLoaders (
+    id      INTEGER PRIMARY KEY,
+    name    TEXT UNIQUE
+);
 
-    CONSTRAINT plugin_ck_enabled CHECK (enabled IN (0, 1))
+CREATE TABLE Plugins (
+    id          INTEGER PRIMARY KEY,
+    loader      TEXT    NOT NULL,
+    path        TEXT    NOT NULL UNIQUE,
+    checksum    TEXT,
+    enabled     INTEGER NOT NULL DEFAULT 1,
+    failureSafe INTEGER NOT NULL DEFAULT 0,
+
+    name        TEXT    NOT NULL,
+    description TEXT,
+    version     TEXT    NOT NULL,
+
+    CONSTRAINT plugins_ck_enabled CHECK (enabled IN (0, 1)),
+    CONSTRAINT plugins_ck_failureSafe CHECK (failureSafe IN (0, 1)),
+    CONSTRAINT plugins_fk_state FOREIGN KEY (loader)
+        REFERENCES PluginLoaders(name)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE PluginTypes (
+    id          INTEGER PRIMARY KEY,
+    type        TEXT    UNIQUE,
+    description TEXT    NOT NULL
+);
+
+CREATE TABLE PluginType (
+    id          INTEGER PRIMARY KEY,
+    pluginId    INTEGER NOT NULL,
+    type        TEXT    NOT NULL,
+
+    CONSTRAINT plugintype_fk_pluginId FOREIGN KEY (pluginId)
+        REFERENCES Plugins(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT plugintype_fk_type FOREIGN KEY (type)
+        REFERENCES PluginTypes(type)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT plugintype_ck_uniq UNIQUE (pluginId, type)
 );
 
 CREATE TABLE ExecutionStates (
@@ -48,17 +86,17 @@ CREATE TABLE JobExecutions (
     state       TEXT    NOT NULL,
     context     TEXT,
 
-    CONSTRAINT jobexecutions_ck_unique UNIQUE (jobId, pluginId)
+    CONSTRAINT jobexecutions_ck_unique UNIQUE (jobId, pluginId),
     CONSTRAINT jobexecutions_fk_jobId FOREIGN KEY (jobId)
-        REFERENCES Job(id)
+        REFERENCES Jobs(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT jobexecutions_fk_pluginId FOREIGN KEY (pluginId)
-        REFERENCES Plugin(id)
+        REFERENCES Plugins(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT jobexecutions_fk_jobState FOREIGN KEY (jobState)
-        REFERENCES States(state)
+        REFERENCES JobStates(state)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT jobexecutions_fk_state FOREIGN KEY (state)
@@ -73,26 +111,39 @@ CREATE TABLE Preferences (
     value       TEXT
 );
 
-INSERT INTO States (state, name, description) VALUES ('new', 'New', 'This is a new job that has never been executed before.');
-INSERT INTO States (state, name, description) VALUES ('meta', 'Gathering metadata', 'Performing analysis on the files.');
-INSERT INTO States (state, name, description) VALUES ('pre', 'Generating transcoder options', 'Generating options to use with the transcoder, if any for each file');
-INSERT INTO States (state, name, description) VALUES ('filter', 'Filtering options and files', 'Checking that generated options are optimal and removing deletable files.');
-INSERT INTO States (state, name, description) VALUES ('exec', 'Running transcode operations', 'Using ffmpeg to ensure files are in the desired format.');
-INSERT INTO States (state, name, description) VALUES ('post', 'Post-execution analysis', 'Analysing changes that were made and notifying downstream services.');
-INSERT INTO States (state, name, description) VALUES ('complete', 'Complete', 'The job is complete.');
-INSERT INTO States (state, name, description) VALUES ('abort', 'Aborted', 'The job was aborted.');
+INSERT INTO JobStates (state, name, description) VALUES ('new', 'New', 'This is a new job that has never been executed before.');
+INSERT INTO JobStates (state, name, description) VALUES ('meta', 'Gathering metadata', 'Performing analysis on the files.');
+INSERT INTO JobStates (state, name, description) VALUES ('pre', 'Generating transcoder options', 'Generating options to use with the transcoder, if any for each file');
+INSERT INTO JobStates (state, name, description) VALUES ('filter', 'Filtering options and files', 'Checking that generated options are optimal and removing deletable files.');
+INSERT INTO JobStates (state, name, description) VALUES ('exec', 'Running transcode operations', 'Using ffmpeg to ensure files are in the desired format.');
+INSERT INTO JobStates (state, name, description) VALUES ('post', 'Post-execution analysis', 'Analysing changes that were made and notifying downstream services.');
+INSERT INTO JobStates (state, name, description) VALUES ('complete', 'Complete', 'The job is complete.');
+INSERT INTO JobStates (state, name, description) VALUES ('abort', 'Aborted', 'The job was aborted.');
 
 INSERT INTO ExecutionStates (state, name, description) VALUES ('started', 'Plugin Started', 'The plugin has started executing');
 INSERT INTO ExecutionStates (state, name, description) VALUES ('successful', 'Successful', 'Plugin execution was successful');
 INSERT INTO ExecutionStates (state, name, description) VALUES ('failed', 'Failed', 'Plugin execution failed');
 INSERT INTO ExecutionStates (state, name, description) VALUES ('unknown', 'Unknown', 'The plugin is in an unknown state');
 
+INSERT INTO PluginLoaders (name) VALUES ("file");
+INSERT INTO PluginLoaders (name) VALUES ("package");
+
+INSERT INTO PluginTypes (type, description) VALUES ('meta', 'Performs analysis on the files.');
+INSERT INTO PluginTypes (type, description) VALUES ('pre', 'Generates transcoder options');
+INSERT INTO PluginTypes (type, description) VALUES ('filter', 'Filters options and files');
+INSERT INTO PluginTypes (type, description) VALUES ('exec', 'Runs transcode operations');
+INSERT INTO PluginTypes (type, description) VALUES ('post', 'Performs post-execution analysis');
+
 --------------------------------------------------------------------------------
 -- Down
 --------------------------------------------------------------------------------
 
 DROP TABLE JobExecutions;
-DROP TABLE Job;
-DROP TABLE States;
-DROP TABLE Plugin;
+DROP TABLE ExecutionStates;
+DROP TABLE Jobs;
+DROP TABLE JobStates;
+DROP TABLE PluginType;
+DROP TABLE PluginTypes;
+DROP TABLE Plugins;
+DROP TABLE PluginLoaders;
 DROP TABLE Preferences;
