667f2853c82f9ae2e8975cb764377526896e03d0
Matthew Knox <matthew@makereti.co.nz>
2020-05-06 19:40:39 +1000

    Fix Radarr webhooks

--- src/backend/api/v1/radarr.js
+++ src/backend/api/v1/radarr.js
@@ -5,14 +5,19 @@ module.exports = serviceFactory => {
     const directoryService = serviceFactory.getDirectoryService();
     return (req, res) => {
         res.json({});
-
+        
+        if (req.body.eventType === 'Grab') {
+            LOG.error('The "Grab" Radarr webhook type is not supported.');
+            return;
+        }
+        
         if (req.body.eventType === 'Test') {
             LOG.info('Radarr performed a test against the API');
             return;
         }
 
-        const origPath = join(req.body.movie.path, req.body.movieFile.relativePath);
-        const episodePath = directoryService.getRealPath(origPath);
-        executorService.execute(episodePath);
+        const origPath = join(req.body.movie.folderPath, req.body.movieFile.relativePath);
+        const moviePath = directoryService.getRealPath(origPath);
+        executorService.execute(moviePath);
     };
 };
